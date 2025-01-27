7769d1849334792956c8eaaea33bd3dd57bbbcc1
Andrew Arnott <andrewarnott@live.com>
2025-01-27 11:50:16 -0700

    Add the `AggregatingTypeShapeProvider` class (#107)
    * Add `TypeShapeProvider.Combine` static method
    
    Closes #91
    
    * Rollback to C# 12 and use a params array
    
    * Expose the aggregating type shape provider publicly
    
    * Add a defensive copy of the array
    
    Also mention the "chain of responsibility" pattern.
    
    * Ensure `Providers` property is truly immutable
    
    * Avoid a type reference to `ReadOnlyCollection<T>`
    
    * Exclude an uninvoked mock from code coverage

---
+++ src/PolyType/Utilities/AggregatingTypeShapeProvider.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.ObjectModel;
+using PolyType.Abstractions;
+
+namespace PolyType.Utilities;
+
+/// <summary>
+/// An <see cref="ITypeShapeProvider"/> that returns the first shape found
+/// from iterating over a given list of providers.
+/// </summary>
+/// <remarks>
+/// <para>
+/// This aggregator implements the chain of responsibility pattern.
+/// </para>
+/// <para>
+/// Whether this aggregator caches the results of the providers is undefined,
+/// and should be non-observable assuming the providers themselves do not
+/// change their return values over time.
+/// </para>
+/// </remarks>
+public sealed class AggregatingTypeShapeProvider : ITypeShapeProvider
+{
+    private readonly IReadOnlyList<ITypeShapeProvider> _providers;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="AggregatingTypeShapeProvider"/> class.
+    /// </summary>
+    /// <param name="providers">The sequence of providers to solicit for type shapes.</param>
+    public AggregatingTypeShapeProvider(params ITypeShapeProvider[] providers)
+    {
+        if (providers is null)
+        {
+            throw new ArgumentNullException(nameof(providers));
+        }
+
+        foreach (ITypeShapeProvider provider in providers)
+        {
+            if (provider is null)
+            {
+                throw new ArgumentException("One or more providers are null.", nameof(providers));
+            }
+        }
+
+        _providers = [.. providers];
+    }
+
+    /// <summary>
+    /// Gets the list of providers making up this aggregate provider.
+    /// </summary>
+    public IReadOnlyList<ITypeShapeProvider> Providers => _providers;
+
+    /// <inheritdoc/>
+    public ITypeShape? GetShape(Type type)
+    {
+        for (int i = 0; i < _providers.Count; i++)
+        {
+            if (_providers[i].GetShape(type) is ITypeShape shape)
+            {
+                return shape;
+            }
+        }
+
+        return null;
+    }
+}
---
+++ tests/PolyType.Tests/Utilities/AggregatingTypeShapeProviderTests.cs
@@ -0,0 +1,187 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
+using PolyType.Utilities;
+
+namespace PolyType.Tests.Utilities;
+
+public class AggregatingTypeShapeProviderTests
+{
+    [Fact]
+    public void Ctor_Null()
+    {
+        Assert.Throws<ArgumentNullException>(() => new AggregatingTypeShapeProvider(null!));
+    }
+
+    [Fact]
+    public void Ctor_NullElement()
+    {
+        Assert.Throws<ArgumentException>(() => new AggregatingTypeShapeProvider([null!]));
+    }
+
+    [Fact]
+    public void EmptyList()
+    {
+        AggregatingTypeShapeProvider aggregate = new();
+        Assert.Null(aggregate.GetShape(typeof(int)));
+    }
+
+    [Fact]
+    public void One_NullShape()
+    {
+        AggregatingTypeShapeProvider aggregate = new(new MockTypeShapeProvider());
+        Assert.Null(aggregate.GetShape(typeof(int)));
+    }
+
+
+    [Fact]
+    public void One_NonNullShape()
+    {
+        MockTypeShapeProvider first = new()
+        {
+            Shapes =
+            {
+                [typeof(int)] = new MockTypeShape<int>(),
+            },
+        };
+        AggregatingTypeShapeProvider aggregate = new(first);
+        Assert.Same(first.Shapes[typeof(int)], aggregate.GetShape(typeof(int)));
+    }
+
+    [Fact]
+    public void ManyProviders()
+    {
+        MockTypeShapeProvider first = new()
+        {
+            Shapes =
+            {
+                [typeof(int)] = new MockTypeShape<int>(),
+            },
+        };
+        MockTypeShapeProvider second = new()
+        {
+            Shapes =
+            {
+                [typeof(string)] = new MockTypeShape<int>(),
+            },
+        };
+        AggregatingTypeShapeProvider aggregate = new(first, second);
+        Assert.Same(first.Shapes[typeof(int)], aggregate.GetShape(typeof(int)));
+        Assert.Same(second.Shapes[typeof(string)], aggregate.GetShape(typeof(string)));
+        Assert.Null(aggregate.GetShape(typeof(bool)));
+    }
+
+    [Fact]
+    public void DefensiveCopy()
+    {
+        ITypeShapeProvider[] providers =
+        [
+            new MockTypeShapeProvider(),
+        ];
+        AggregatingTypeShapeProvider aggregate = new(providers);
+        providers[0] = new MockTypeShapeProvider
+        {
+            Shapes =
+            {
+                [typeof(int)] = new MockTypeShape<int>(),
+            },
+        };
+
+        // Verify that the new provider is not used.
+        Assert.Null(aggregate.GetShape(typeof(int)));
+    }
+
+    [Fact]
+    public void IterateOverProviders()
+    {
+        AggregatingTypeShapeProvider provider = new(new MockTypeShapeProvider());
+        Assert.Single(provider.Providers);
+    }
+
+    /// <summary>
+    /// Validates that the aggregating provider does not cache results.
+    /// </summary>
+    /// <remarks>
+    /// Whether it caches or not is something of an arbitrary design decision.
+    /// This test is to ensure that the behavior is documented and intentional.
+    /// </remarks>
+    [Fact]
+    public void CachePolicy_ShapeNotCached()
+    {
+        MockTypeShapeProvider first = new()
+        {
+            Shapes =
+            {
+                [typeof(int)] = new MockTypeShape<int>(),
+            },
+        };
+        AggregatingTypeShapeProvider aggregate = new(first);
+        Assert.Same(first.Shapes[typeof(int)], aggregate.GetShape(typeof(int)));
+
+        // Remove the shape, and verify that the aggregator no longer returns it.
+        first.Shapes.Remove(typeof(int));
+        Assert.Null(aggregate.GetShape(typeof(int)));
+    }
+
+    /// <summary>
+    /// Validates that the aggregating provider does not cache the provider that was used previously.
+    /// </summary>
+    /// <remarks>
+    /// Whether it caches or not is something of an arbitrary design decision.
+    /// This test is to ensure that the behavior is documented and intentional.
+    /// </remarks>
+    [Fact]
+    public void CachePolicy_ShapeProviderNotCached()
+    {
+        MockTypeShapeProvider first = new()
+        {
+            Shapes =
+            {
+                [typeof(int)] = new MockTypeShape<int>(),
+            },
+        };
+        MockTypeShapeProvider second = new()
+        {
+            Shapes =
+            {
+                [typeof(string)] = new MockTypeShape<string>(),
+            },
+        };
+        AggregatingTypeShapeProvider aggregate = new(first, second);
+        Assert.NotNull(aggregate.GetShape(typeof(string)));
+
+        // Add a new string shape to the first provider.
+        first.Shapes[typeof(string)] = new MockTypeShape<string>();
+
+        // The aggregator should return the new shape.
+        Assert.Same(first.Shapes[typeof(string)], aggregate.GetShape(typeof(string)));
+    }
+
+    private class MockTypeShapeProvider : ITypeShapeProvider
+    {
+        internal Dictionary<Type, ITypeShape> Shapes { get; } = new();
+
+        public ITypeShape? GetShape(Type type) => Shapes.TryGetValue(type, out ITypeShape? shape) ? shape : null;
+    }
+
+    [ExcludeFromCodeCoverage]
+    private class MockTypeShape<T> : ITypeShape<T>
+    {
+        public Type Type => throw new NotImplementedException();
+
+        public TypeShapeKind Kind => throw new NotImplementedException();
+
+        public ITypeShapeProvider Provider => throw new NotImplementedException();
+
+        public ICustomAttributeProvider? AttributeProvider => throw new NotImplementedException();
+
+        public object? Accept(ITypeShapeVisitor visitor, object? state = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object? Invoke(ITypeShapeFunc func, object? state = null)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
