2a980d35efdbf17c89ba17a19949c9aec3d6d442
Eirik Tsarpalis <eirik.tsarpalis@gmail.com>
2025-09-10 15:15:27 +0300

    Fix global namespace handling in the source generator. (#261)

---
+++ src/PolyType.TestCases/TestTypes.GlobalNamespace.cs
@@ -0,0 +1,13 @@
+﻿using PolyType;
+
+#pragma warning disable CA1050 // Declare types in namespaces
+
+[GenerateShape]
+public partial record RecordWithoutNamespace(int Value);
+
+public record GenericRecordWithoutNamespace<T>(T Value);
+
+public static class GenericContainerWithoutNamespace<T1>
+{ 
+    public partial record Record<T2>(T1 value1, T2 value2);
+}
